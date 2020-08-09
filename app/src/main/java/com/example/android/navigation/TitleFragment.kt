b4b7064140e8e75f98a5b6bc03d6aa54688527af
Sorravit4425 <65063632+Sorravit4425@users.noreply.github.com>
2020-08-09 21:19:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,42 @@
+package com.example.android.navigation
+
+import android.database.DatabaseUtils
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater,
+        R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener {
+            view: View ->  Navigation.findNavController(view).navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
+        }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater?.inflate(R.menu.overflow_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+                requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+}
